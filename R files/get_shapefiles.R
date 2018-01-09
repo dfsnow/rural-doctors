2d5286e7f927811fa678759b4bd7aeff271c24a4
Dan Snow <dfsnow@uchicago.edu>
2018-01-08 22:02:12 -0600

    Initial commit

---
+++ R files/get_shapefiles.R
@@ -0,0 +1,50 @@
+library(tigris)
+library(maps)
+library(purrr)
+library(rgdal)
+library(glue)
+
+# Use this variable to alter the year of the shapefile downloads
+needed_year <- 2017
+
+# Using the maps package to get all state FIPS codes into a dataframe
+data(state.fips)
+
+
+######## PUMAs ########
+# Using the tigris package to download TIGER/Line files for PUMAs for
+# each state, then combine them into a single shapefile/layer
+glue('ti_{y}_us_pumas', y = needed_year) <- rbind_tigris(
+  state.fips$fips %>%
+    map(pumas, year = needed_year))
+
+
+######## CBSA/MSA ########
+# Using the tigris package to download all the Core Based
+# Statistical Areas (Metropolitan/Micropolitan Statistical Areas)
+# for the needed year. Used assign() because of bug in tigris
+assign(glue('ti_{y}_us_cbsa', y = needed_year), core_based_statistical_areas(
+  year = needed_year))
+
+
+######## Urban Areas ########
+# Using the tigris package to download all US urban areas
+assign(glue('ti_{y}_us_uac10', y = needed_year), urban_areas(
+  year = needed_year))
+
+
+######## Saving ########
+# Saving all the resulting shapefiles to disk for use in Python or QGIS
+spatial_df <- mget(ls(pattern = "_us_"))
+spatial_df_names <- names(spatial_df)
+
+spatial_df_names %>%
+walk(function(x) {
+  data <- spatial_dataframes[[x]]
+  writeOGR(
+    data,
+    dsn = "tempdir",
+    layer = x,
+    overwrite_layer = TRUE,
+    driver = "ESRI Shapefile")
+})
