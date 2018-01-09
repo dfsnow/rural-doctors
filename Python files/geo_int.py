73fa9e187a9d2a2f3ace444cb6b6cbfca73d2000
Dan Snow <dfsnow@uchicago.edu>
2018-01-09 00:49:08 -0600

    Working but very slow prototype intersection script

--- Python files/geo_int.py
+++ Python files/geo_int.py
@@ -1,12 +1,9 @@
 import os
 import geopandas as gpd
-import numpy as np
-from shapely.geometry import LineString
-import pandas as pd
-import seaborn as sns
 
-batch_size = 10
+
 year_needed = 2017
+batch_size = 10
 
 shapefiles_dir = os.path.expanduser(
     "~/Harris Drive/Research/Projects/Rural Doctors/Shapefiles/")
@@ -15,22 +12,14 @@ puma = gpd.read_file(os.path.join(shapefiles_dir, 'ti_' + str(year_needed) + '_u
 cbsa = gpd.read_file(os.path.join(shapefiles_dir, 'ti_' + str(year_needed) + '_us_cbsa.shp'))
 # urban = gpd.read_file(os.path.join(shapefiles_dir, 'ti_' + str(year_needed) + '_us_uac10.shp'))
 
-row_cnt_cbsa = len(cbsa)
-iterations_cbsa = int(np.ceil(row_cnt / b))
+final = []
 
-row_cnt_puma = len(puma)
-iterations_puma = int(np.ceil(row_cnt / b))
-
-start_idx = 0
-end_idx = start_idx + b
-
-final = gpd.GeoDataFrame()
-
-
-for orig in cbsa.iterrows():
-    for ref in puma.iterrows():
+for index, orig in puma.iterrows():
+    for index2, ref in cbsa.iterrows():
         if ref['geometry'].intersects(orig['geometry']):
-         MSA_NAME = orig['NAME']
-         data.append({'geometry':ref['geometry'].intersection(orig['geometry']),'MSA_NAME':MSA_NAME})
+            MSA_NAME = ref['NAME']
+            final.append({'geometry':ref['geometry'].intersection(orig['geometry']),'TEST':MSA_NAME})
+    print("Iteration: %s/%s" % (index, len(puma)))
 
-cbsa.to_file("test.shp")
+
+final.to_file(shapefiles_dir, driver="ESRI Shapefile")
