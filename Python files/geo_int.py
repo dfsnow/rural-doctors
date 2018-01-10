105d3d0d8835760823c5b68d313133e70eceeff4
Dan Snow <dfsnow@uchicago.edu>
2018-01-09 22:32:29 -0600

    Updated file saving

--- Python files/geo_int.py
+++ Python files/geo_int.py
@@ -14,17 +14,17 @@ cbsa_pop['GEOID'] = cbsa_pop['GEOID'].astype(str)
 # Read PUMA, CBSA, and Urban Area shapefiles from the shapefile dir for the specified year
 puma = gpd.read_file(os.path.join(shapefiles_dir, 'ti_' + str(year_needed) + '_us_pumas.shp'))
 cbsa = gpd.read_file(os.path.join(shapefiles_dir, 'ti_' + str(year_needed) + '_us_cbsa.shp'))
-# urban = gpd.read_file(os.path.join(shapefiles_dir, 'ti_' + str(year_needed) + '_us_uac10.shp'))
+urban = gpd.read_file(os.path.join(shapefiles_dir, 'ti_' + str(year_needed) + '_us_uac10.shp'))
 
 
 ######## Spatial Processing ########
 # Perform spatial join on PUMAs and CBSAs where intersecting
 int_puma_cbsa = gpd.sjoin(puma, cbsa, how='inner', op='intersects')
-# int_urban_cbsa = gpd.sjoin(urban, cbsa, how='inner', op='intersects')
+int_urban_cbsa = gpd.sjoin(urban, cbsa, how='inner', op='intersects')
 
 # Merge CBSA population data with spatial data files
 merged_puma_cbsa = pd.DataFrame(cbsa_pop.merge(int_puma_cbsa, on='GEOID'))
-# merged_urban_cbsa = pd.DataFrame(cbsa_pop.merge(int_urban_cbsa, on='GEOID'))
+merged_urban_cbsa = pd.DataFrame(cbsa_pop.merge(int_urban_cbsa, on='GEOID'))
 
 # Filter the merged data, keeping only intersecting CBSAs with the highest population
 final_puma = gpd.GeoDataFrame(
@@ -32,14 +32,18 @@ final_puma = gpd.GeoDataFrame(
         merged_puma_cbsa['2016'] ==
         merged_puma_cbsa.groupby('GEOID10')['2016'].transform(max)])
 
-# final_urban = gpd.GeoDataFrame(
-#     merged_urban_cbsa[
-#         merged_urban_cbsa['2016'] ==
-#         merged_urban_cbsa.groupby(['GEOID10'])['2016'].transform(max)])
+final_urban = gpd.GeoDataFrame(
+    merged_urban_cbsa[
+        merged_urban_cbsa['2016'] ==
+        merged_urban_cbsa.groupby(['GEOID10'])['2016'].transform(max)])
 
 
 ######## Exporting ########
 # Save final PUMA shapefiles
-final_puma.to_file(shapefiles_dir, driver="ESRI Shapefile")
-# final_urban.to_file(shapefiles_dir, driver="ESRI Shapefile")
+final_puma.to_file(
+    os.path.join(shapefiles_dir, 'ti_' + str(year_needed) + '_us_pumas_MERGED.shp'),
+    driver="ESRI Shapefile")
+final_urban.to_file(
+    os.path.join(shapefiles_dir, 'ti_' + str(year_needed) + '_us_urban_MERGED.shp'),
+    driver="ESRI Shapefile")
 
